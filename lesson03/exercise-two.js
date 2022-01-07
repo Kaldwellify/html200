ebaf377dafbb63695828c06996365580387b4988
Kennedy Caldwell <kaldwell95@gmail.com>
2022-01-07 05:06:14 -0800

    first commit

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,5 @@
+let mySentence = 'I like to code with JavaScript.'; // Make a string variable of a sentence with least five words and a period.
+
+let myArray = mySentence.split(" "); // Make an array from that sentence.
+
+console.log(myArray[2]); // Log the 3rd word to the console.
