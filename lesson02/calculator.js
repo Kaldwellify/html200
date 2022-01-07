1756217a67a19a00167dd8a06001b617f67b2962
Kennedy Caldwell <87290926+Kaldwellify@users.noreply.github.com>
2022-01-07 04:12:29 -0800

    added last 2 output comments

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -6,5 +6,5 @@ let z = 99;
 
 console.log(w + x); //outputs 40
 console.log(y - z); //outputs -87
-console.log(z / x + y); //
-console.log(w * x + y - z);
+console.log(z / x + y); //outputs 16.304347826086957
+console.log(w * x + y - z); //outputs 304
